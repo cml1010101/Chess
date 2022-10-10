16cb98be8689121a12ffda8322805acae4ab8cc0
cml1010101 <cml1010101@gmail.com>
2022-10-10 09:37:38 -0400

    commit

--- src/Chess.cpp
+++ src/Chess.cpp
@@ -200,8 +200,8 @@ bool Queen::canMove(Board* board, Move* move)
             {
                 if (board->grid[loc.row][loc.col + i]) return false;
             }
-            return board->grid[loc.row][move->dest.col] == NULL
-                || board->grid[loc.row][move->dest.col]->player != player;
+            return board->grid[move->dest.row][move->dest.col] == NULL
+                || board->grid[move->dest.row][move->dest.col]->player != player;
         }
         else if (dCol < 0)
         {
@@ -209,8 +209,8 @@ bool Queen::canMove(Board* board, Move* move)
             {
                 if (board->grid[loc.row][loc.col - i]) return false;
             }
-            return board->grid[loc.row][move->dest.col] == NULL
-                || board->grid[loc.row][move->dest.col]->player != player;
+            return board->grid[move->dest.row][move->dest.col] == NULL
+                || board->grid[move->dest.row][move->dest.col]->player != player;
         }
     }
     else if (dCol == 0)
